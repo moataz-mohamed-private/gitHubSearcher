d6b0823d7f20fc5ce732cd19117217abb402e601
moatazmohamedmm <moataz.mohamed@sylndr.com>
2024-07-28 13:07:24 +0300

    cards styling nearly done

--- src/components/cardFactory/repoCard/repo-card.tsx
+++ src/components/cardFactory/repoCard/repo-card.tsx
@@ -2,78 +2,59 @@ import { FC } from "react";
 // import { FaStar, FaCircle } from "react-icons/fa";
 // import { BiGitRepoForked } from "react-icons/bi";
 // import { BsEye } from "react-icons/bs";
+import {
+  StarIcon,
+  EyeOpenIcon,
+  CommitIcon,
+  CodeIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import { BiGitRepoForked } from "@radix-ui/react-icons/bi";
+import { BsEye } from "@radix-ui/react-icons/bs";
 import { RepoItem } from "@/types/repos";
 import classes from "../styles.module.scss";
+import { Link } from "react-router-dom";
 
 type CardProps = {
   item: RepoItem;
 };
 
 export const RepoCard: FC<CardProps> = ({ item }) => {
+  const openInNewTab = (url: string) => {
+    window.open(url, "_blank", "noreferrer");
+  };
   return (
-    <div className={classes.cardList}>
+    <div
+      className={classes.repoCard}
+      onClick={() => openInNewTab(item.html_url)}
+    >
       <article className={classes.card}>
-        <p>{item.description}</p>
+        <p className={classes.description}>{item.description}</p>
         <div className={classes.cardHeader}>
-          <a href="#">{item.name}</a>
-          <button className={classes.iconButton}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              display="block"
-              id="Heart"
-            >
-              <path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
-            </svg>
-          </button>
+          <h3 className={classes.name}>{item.name}</h3>
+          <div>
+            <button className={classes.iconButton}>
+              <StarIcon />
+            </button>
+            <div className={classes.StarsCount}>{item.stargazers_count}</div>
+          </div>
         </div>
         <div className={classes.cardFooter}>
           <div className={classes.cardMeta}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              display="block"
-              id="EyeOpen"
-            >
-              <path d="M21.257 10.962c.474.62.474 1.457 0 2.076C19.764 14.987 16.182 19 12 19c-4.182 0-7.764-4.013-9.257-5.962a1.692 1.692 0 0 1 0-2.076C4.236 9.013 7.818 5 12 5c4.182 0 7.764 4.013 9.257 5.962z" />
-              <circle cx="12" cy="12" r="3" />
-            </svg>
-            2,465
+            <EyeOpenIcon />
+            {item.watchers_count}
           </div>
           <div className={classes.cardMeta}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              display="block"
-              id="Calendar"
-            >
-              <rect x="2" y="4" width="20" height="18" rx="4" />
-              <path d="M8 2v4" />
-              <path d="M16 2v4" />
-              <path d="M2 10h20" />
-            </svg>
-            Jul 26, 2019
+            <CommitIcon />
+            {item.forks_count}
+          </div>
+          <div className={classes.cardMeta}>
+            <CodeIcon />
+            {item.language}
+          </div>
+          <div className={classes.cardMeta}>
+            <PersonIcon />
+            {item.owner.login}
           </div>
         </div>
       </article>
--- src/components/searchResults/index.tsx
+++ src/components/searchResults/index.tsx
@@ -4,12 +4,13 @@ import {
 } from "@/store/gitHubSearch/gitHubSearch.selectors";
 import { FC } from "react";
 import { CardFactory } from "../cardFactory";
+import classes from "./styles.module.scss";
 
 const SearchResults: FC = () => {
   const searchResults = useSearchResult();
   const filterType = useFilterType();
   return (
-    <div>
+    <div className={classes.CardsGrid}>
       {searchResults?.items?.map((item) => (
         <CardFactory type={filterType} item={item} />
       ))}
